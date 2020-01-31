addac28e744a957bef0b1fd437a1d8069d1f84c0
Rokcas <rokas0090@gmail.com>
2020-02-01 00:02:33 +0200

    Add horizontal movement

--- Player.gd
+++ Player.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if velocity.length() > 0:	#Not needed ATM
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
