e6cf1f71c498193e2df97188ed012299c1cc02ea
Simon <simonas.petkus9@gmail.com>
2020-02-02 14:05:35 +0200

    Fade to black

--- entities/scene_changer/scene_changer.gd
+++ entities/scene_changer/scene_changer.gd
@@ -2,5 +2,15 @@ extends Area2D
 
 export(String, FILE, "*.tscn") var target_scene
 
+var time_passed = -1;
+
 func interact():
-	get_tree().change_scene(target_scene)
+	get_parent().get_node("Fader/AnimationPlayer").play("FadeOut")
+	time_passed = 0;
+
+func _process(delta):
+	if time_passed >= 0:
+		time_passed += delta
+	if time_passed >= 1:
+		get_tree().change_scene(target_scene)
+		time_passed = -1
