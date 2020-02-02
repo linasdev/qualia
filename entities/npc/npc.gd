4c31bf4b335ce089d3bed47a2f3a6f56da0f0c56
omniclown <=markascerniauskas@gmail.com>
2020-02-02 11:13:14 +0200

    scene transition example

--- entities/npc/npc.gd
+++ entities/npc/npc.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 export (String) var sample_text
 
 func interact():
+	get_tree().change_scene("res://scenes/scene3(atlas)/scene3(atlas).tscn")
 	$dialogue_bubble/Label.text = sample_text
 	$dialogue_bubble.show()
 	get_parent().get_node("scene_changer").show()
