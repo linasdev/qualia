bf83e69098e1ffd290f2f83528eb0ca83b3758cb
Simon <simonas.petkus9@gmail.com>
2020-02-02 16:13:03 +0200

    buttons

---
+++ scenes/Tittle screen/TextureButton.gd
@@ -0,0 +1,18 @@
+extends TextureButton
+
+var time_passed = -1;
+
+func _pressed():
+	get_parent().get_node("Fader/ColorRect").show()
+	get_parent().get_node("Fader/AnimationPlayer").play("FadeOut")
+	time_passed = 0
+	
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if time_passed >= 0:
+		time_passed += delta
+	if time_passed >= 1:
+		get_tree().change_scene("res://scenes/scene6(forest)/scene6(forest).tscn")
+		time_passed = -1
---
+++ scenes/Tittle screen/TextureButton2.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+
+func _pressed():
+	get_tree().quit()
+
+func _ready():
+	pass # Replace with function body.
+
